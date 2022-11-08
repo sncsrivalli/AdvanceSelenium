7799fe4e0505c0009108f3e78067afa25d884c5c
srivalli <snc.srivalli1@gmail.com>
2022-11-08 07:55:12 +0530

    ghmjn

--- src/test/java/scripts/Test0.java
+++ src/test/java/scripts/Test0.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 
 //changes made in local disc D
 
->>>>>>> branch 'master' of https://github.com/sncsrivalli/AdvanceSelenium.git
 public class Test0 {
 	
 	@Test
